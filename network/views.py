ace08181f287c9612dcadd289ce4a3d44412c879
Iv-EN <engenivanov@yandex.ru>
2025-10-07 21:51:06 +0300

    wrote a ViewSet for NetworkObject

--- network/views.py
+++ network/views.py
@@ -3,3 +3,10 @@ from django_filters.rest_framework import DjangoFilterBackend
 from .models import NetworkObject
 from .serializers import NetworkObjectSerializer
 
+
+class NetworkObjectViewSet(viewsets.ModelViewSet):
+    queryset = NetworkObject.objects.all()
+    serializer_class = NetworkObjectSerializer
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ['country']
+    ordering_fields = ['name', 'country']
